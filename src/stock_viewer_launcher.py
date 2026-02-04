f6bf81ec2f1033eb1be8b85fe7ff9da65abad438
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2026-02-04 10:26:15 -0300

    pyinstaller

---
+++ src/stock_viewer_launcher.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# pip install pyinstaller
+# pyinstaller --onefile --windowed --name stock_viewer stock_viewer_launcher.py
+
+from stock_viewer.prog_viewer import main
+
+if __name__ == "__main__":
+    main()
+
