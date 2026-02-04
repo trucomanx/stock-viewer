c249bd03db610855c1b716b7b0bc24b2f5fdccf9
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2026-02-04 11:56:42 -0300

    mejorando pyinstaller

--- src/stock_viewer_launcher.py
+++ src/stock_viewer_launcher.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 
 # pip install pyinstaller
-# pyinstaller --onefile --windowed --name stock_viewer stock_viewer_launcher.py
+
+# python3 -m venv venv-stock-viewer
+# source venv-stock-viewer/bin/activate
+# pip install --upgrade pip
+# pip install pyinstaller
+# pip install -r requirements.txt
+# cd src
+#
+# ### windows ##
+# python3 -m PyInstaller --onefile --windowed --name stock_viewer --add-data "stock_viewer/icons;icons"  stock_viewer_launcher.py
+#
+# ### ubuntu ##
+# python3 -m PyInstaller --onefile --windowed --name stock_viewer --add-data "stock_viewer/icons:icons"  stock_viewer_launcher.py
 
 from stock_viewer.prog_viewer import main
 
